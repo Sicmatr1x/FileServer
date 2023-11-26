7b9141e8b44ef00811ac163880f13308b41fc9b2
Sicmatr1x <sicmatr1x@outlook.com>
2023-11-26 14:26:32 +0800

    Create sendGETList.js

---
+++ sendGETList.js
@@ -0,0 +1,87 @@
+const list = [ {
+  "seq" : 0,
+  "context" : "IyBBbnRpYm9kaWVzDQoNCkNoYXJsZXMgU3Ryb3NzDQoNCj4g6K+R5paH5p2C5b+X5Y+R6KGo77ya56eR5bm75LiW55WMMjAxMOW5tDA35pyf44CK5oqX5L2T44CLW+iLsV0g5p+l5bCU5pavwrfmlq-__-nibnnvZfmlq8gW+iRl10g5Lit55+t56+H5bCP6K+0ICDlhajmlofliIrlvZUNCg0KQWx0aG91Z2ggaGUgbWFkZSBoaXMgZml"
+}, {
+  "seq" : 1,
+  "context" : "yc3Qgc2FsZSBiYWNrIGluIDE5ODcsIGl04oCZcyBvbmx5IHJlY2VudGx5IHRoYXQgQnJpdGlzaCB3cml0ZXIgQ2hhcmxlcyBTdHJvc3MgaGFzIGJlZ3VuIHRvIG1ha2UgYSBuYW1lIGZvciBoaW1zZWxmIGFzIGEgd3JpdGVyIHRvIHdhdGNoIGluIHRoZSBuZXcgY2VudHVyeSBhaGVhZCAoaW4gZmFjdCwgYXMgb25lIG9mIHRoZSBrZ"
+}, {
+  "seq" : 2,
+  "context" : "XkgV3JpdGVycyB0byBXYXRjaCBpbiB0aGUgT3VnaHRzKSwgd2l0aCBhIHN1ZGRlbiBidXJzdCBpbiB0aGUgbGFzdCBmZXcgeWVhcnMgb2YgcXVpcmt5LCBpbnZlbnRpdmUsIGhpZ2gtYml0LXJhdGUgc3RvcmllcyBzdWNoIGFzIOKAnEEgQ29sZGVyIFdhcizigJ0g4oCcQmVhciBUcmFwLOKAnSDigJxEZWNobG9yaW5hdGluZyB0aGU"
+}, {
+  "seq" : 3,
+  "context" : "gTW9kZXJhdG9yLOKAnSDigJxUb2FzdDogQSBDb24gUmVwb3J0LOKAnSDigJxMb2JzdGVycyzigJ0g4oCcVHJvdWJhZG91cizigJ0g4oCcVG91cmlzdCzigJ0g4oCcSGFsbyzigJ0g4oCcUm91dGVyLOKAnSBhbmQg4oCcTmlnaHRmYWxsLOKAnSBpbiBtYXJrZXRzIHN1Y2ggYXMgSW50ZXJ6b25lLCBTcGVjdHJ1bSBTRiwgQXNpbW924"
+}, {
+  "seq" : 4,
+  "context" : "oCZcyBTY2llbmNlIEZpY3Rpb24gTWFnYXppbmUsIE9keXNzZXksIFN0cmFuZ2UgUGxhc21hLCBhbmQgTmV3IFdvcmxkcy4gU3Ryb3NzIGlzIGFsc28gYSByZWd1bGFyIGNvbHVtbmlzdCBmb3IgdGhlIG1vbnRobHkgbWFnYXppbmUgQ29tcHV0ZXIgU2hvcHBlci4gSGUgaGFzIOKAnHB1Ymxpc2hlZOKAnSBhIG5vdmVsIG9ubGluZSw"
+}, {
+  "seq" : 5,
+  "context" : "gU2NyYXRjaCBNb25rZXksIGF2YWlsYWJsZSB0byBiZSByZWFkIG9uIGhpcyB3ZWJzaXRlICh3d3cuYW50aXBvcGUub3JnL2NoYXJsaWUvKSwgYW5kIHNlcmlhbGl6ZWQgYW5vdGhlciBub3ZlbCwgVGhlIEF0cm9jaXR5IEFyY2hpdmUsIGluIHRoZSBtYWdhemluZSBTcGVjdHJ1bSBTRi4gSGlzIG1vc3QgcmVjZW50IGJvb2sgaXMga"
+}, {
+  "seq" : 6,
+  "context" : "GlzIGZpcnN0IGNvbGxlY3Rpb24sIFRvYXN0LCBhbmQgT3RoZXIgQnVybmVkIE91dCBGdXR1cmVzLCBhbmQgY29taW5nIHVwIGlzIGEgbWFqb3IgbmV3IG5vdmVsLCBTaW5ndWxhcml0eSBTa3kuIEhl4oCZcyBiZWVuIG9uIHRoZSBIdWdvIEZpbmFsIEJhbGxvdCB0d2ljZSBpbiB0aGUgbGFzdCB0d28geWVhcnM6IGluIDIwMDIgd2l"
+}, {
+  "seq" : 7,
+  "context" : "0aCBoaXMgc3Rvcnkg4oCcTG9ic3RlcnPigJ0gYW5kIGluIDIwMDMgd2l0aCDigJxIYWxvLuKAnSBIZSBsaXZlcyBpbiBFZGluYnVyZ2gsIFNjb3RsYW5kLg0KDQpJbiB0aGUgZmFzdC1wYWNlZCBhbmQgaW5ub3ZhdGl2ZSBzdG9yeSB0aGF0IGZvbGxvd3MsIGhlIHN1Z2dlc3RzIHRoYXQgYnkgdGhlIHRpbWUgeW91IGNhbiBiZWdpb"
+}, {
+  "seq" : 8,
+  "context" : "iB0byB3b3JyeSB0aGF0IGl0IG1pZ2h0IG5vdCBiZSBhIGdvb2QgaWRlYSB0byBjcmVhdGUgYSBzdXBlcmludGVsbGlnZW50IEEuSS4sIGl0IG1heSBhbHJlYWR5IGJlIHRvbyBsYXRlIHRvIGRvIGFueXRoaW5nIGFib3V0IGl0Lg0KDQo="
+} ];
+
+// ---------------------------------------------------------------
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sendGetRequest(url) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        // Add any headers if needed
+      },
+    });
+    // console.log('response=', response)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+// Example usage:
+const apiUrl = "http://192.168.50.95:7010/mcafee";
+const filename = "20231112";
+
+async function main() {
+  let lastData;
+  for (let i = 0; i < list.length; i++) { // list.length
+    let sendFlag = false;
+    if (lastData && lastData.success && lastData.data && lastData.data.stillNeed instanceof Array) {
+      const stillNeedArray = lastData.data.stillNeed;
+        if (stillNeedArray.length > 0) {
+          for (let j = 0; j < stillNeedArray.length; j++) {
+            if (i === stillNeedArray[j]) {
+              sendFlag = true;
+              break;
+            }
+          }
+        } else {
+          break;
+        }
+    } else {
+      sendFlag = true;
+    }
+    
+    let item = list[i];
+    let currUrl = apiUrl + "/f/" + filename + "/s/" + list.length + "/seq/" + item.seq + "/b/" + item.context;
+    if (sendFlag) {
+      const data = await sendGetRequest(currUrl);
+      // console.log('data=', data);
+      console.log('data=', data.data);
+      lastData = data;
+    }
+    await sleep(90);
+  }
+}
+main();
